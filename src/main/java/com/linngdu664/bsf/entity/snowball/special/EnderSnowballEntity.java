7d349e5af0fa89a95efc631b2b556b568a997879
zx1316 <1315862279@qq.com>
2023-07-11 18:13:12 +0800

    尝试修复玩家没被传送的bug

--- src/main/java/com/linngdu664/bsf/entity/snowball/special/EnderSnowballEntity.java
+++ src/main/java/com/linngdu664/bsf/entity/snowball/special/EnderSnowballEntity.java
@@ -4,7 +4,7 @@ import com.linngdu664.bsf.entity.BSFSnowballEntity;
 import com.linngdu664.bsf.item.ItemRegister;
 import com.linngdu664.bsf.util.LaunchFunc;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -18,6 +18,8 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
 
 public class EnderSnowballEntity extends BSFSnowballEntity {
     public EnderSnowballEntity(LivingEntity livingEntity, Level level, LaunchFunc launchFunc) {
@@ -35,7 +37,9 @@ public class EnderSnowballEntity extends BSFSnowballEntity {
             Entity entity = pResult.getEntity();
             if (entity instanceof Player || entity instanceof Mob) {
                 Entity owner = getOwner();
-                double x = owner.getX(), y = owner.getY(), z = owner.getZ();
+                System.out.println(owner);
+//                double x = owner.getX(), y = owner.getY(), z = owner.getZ();
+                Vec3 ownerPos = owner.position();
                 Vec3 v1 = owner.getDeltaMovement();
                 Vec3 v2 = entity.getDeltaMovement();
                 float xRot1 = owner.getXRot();
@@ -46,19 +50,18 @@ public class EnderSnowballEntity extends BSFSnowballEntity {
                 owner.setYRot(yRot2);
                 entity.setXRot(xRot1);
                 entity.setYRot(yRot1);
-                if (!level.isClientSide) {
-                    owner.moveTo(entity.getX(), entity.getY(), entity.getZ());
-                    owner.setDeltaMovement(v2);
-                    if (owner instanceof ServerPlayer serverPlayer) {
-                        serverPlayer.connection.send(new ClientboundSetEntityMotionPacket(owner));
-                    }
-                    //owner.push(v2.x,v2.y,v2.z);
-                    //entity.push(v1.x,v1.y,v1.z);
-                    ((ServerLevel) level).sendParticles(ParticleTypes.PORTAL, entity.getX(), entity.getEyeY(), entity.getZ(), 32, 1, 1, 1, 0.1);
-                    ((ServerLevel) level).sendParticles(ParticleTypes.PORTAL, owner.getX(), owner.getEyeY(), owner.getZ(), 32, 1, 1, 1, 0.1);
-                    this.discard();
+                owner.moveTo(entity.position());
+                owner.setDeltaMovement(v2);
+                if (owner instanceof ServerPlayer serverPlayer) {
+                    serverPlayer.connection.send(new ClientboundPlayerPositionPacket(entity.getX(), entity.getY(), entity.getZ(), yRot2, xRot2, new HashSet<>(), owner.getId()));
+                    serverPlayer.connection.send(new ClientboundSetEntityMotionPacket(owner));
                 }
-                entity.moveTo(x, y, z);
+                //owner.push(v2.x,v2.y,v2.z);
+                //entity.push(v1.x,v1.y,v1.z);
+                ((ServerLevel) level).sendParticles(ParticleTypes.PORTAL, entity.getX(), entity.getEyeY(), entity.getZ(), 32, 1, 1, 1, 0.1);
+                ((ServerLevel) level).sendParticles(ParticleTypes.PORTAL, owner.getX(), owner.getEyeY(), owner.getZ(), 32, 1, 1, 1, 0.1);
+                this.discard();
+                entity.moveTo(ownerPos);
                 entity.setDeltaMovement(v1);
             }
         }
