6d254b8f89da2267fcd285998c1841a81d6e0ed3
linngdu664 <664774652@qq.com>
2023-07-13 16:27:37 +0800

    大重构续：实体注册表重构

--- src/main/java/com/linngdu664/bsf/entity/EntityRegister.java
+++ src/main/java/com/linngdu664/bsf/entity/EntityRegister.java
@@ -1,12 +1,17 @@
 package com.linngdu664.bsf.entity;
 
 import com.linngdu664.bsf.Main;
-import com.linngdu664.bsf.entity.snowball.nomal.CompactedSnowballEntity;
-import com.linngdu664.bsf.entity.snowball.nomal.ExplosiveSnowballEntity;
+import com.linngdu664.bsf.entity.snowball.force.MonsterGravitySnowballEntity;
+import com.linngdu664.bsf.entity.snowball.force.MonsterRepulsionSnowballEntity;
+import com.linngdu664.bsf.entity.snowball.force.ProjectileGravitySnowballEntity;
+import com.linngdu664.bsf.entity.snowball.force.ProjectileRepulsionSnowballEntity;
+import com.linngdu664.bsf.entity.snowball.nomal.*;
+import com.linngdu664.bsf.entity.snowball.special.*;
+import com.linngdu664.bsf.entity.snowball.tracking.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -23,12 +28,55 @@ public class EntityRegister {
             ENTITY_TYPES.register("bsf_snowball", () -> EntityType.Builder.<BSFSnowballEntity>of(BSFSnowballEntity::new, MobCategory.MISC)
                     .sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10)
                     .build(new ResourceLocation(Main.MODID, "bsf_snowball").toString()));
-    public static final RegistryObject<EntityType<CompactedSnowballEntity>> COMPACTED_SNOWBALL =
-            ENTITY_TYPES.register("compacted_snowball", () -> EntityType.Builder.<CompactedSnowballEntity>of(CompactedSnowballEntity::new, MobCategory.MISC)
-                    .sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10)
-                    .build(new ResourceLocation(Main.MODID, "compacted_snowball").toString()));
-    public static final RegistryObject<EntityType<ExplosiveSnowballEntity>> EXPLOSIVE_SNOWBALL =
-            ENTITY_TYPES.register("explosive_snowball", () -> EntityType.Builder.<ExplosiveSnowballEntity>of(ExplosiveSnowballEntity::new, MobCategory.MISC)
-                    .sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10)
-                    .build(new ResourceLocation(Main.MODID, "explosive_snowball").toString()));
+
+    public static final RegistryObject<EntityType<SmoothSnowballEntity>> SMOOTH_SNOWBALL = registerTool(SmoothSnowballEntity::new,"smooth_snowball");
+    public static final RegistryObject<EntityType<CompactedSnowballEntity>> COMPACTED_SNOWBALL = registerTool(CompactedSnowballEntity::new,"compacted_snowball");
+//    public static final RegistryObject<EntityType<CompactedSnowballEntity>> COMPACTED_SNOWBALL =
+//            ENTITY_TYPES.register("compacted_snowball", () -> EntityType.Builder.<CompactedSnowballEntity>of(CompactedSnowballEntity::new, MobCategory.MISC)
+//                    .sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10)
+//                    .build(new ResourceLocation(Main.MODID, "compacted_snowball").toString()));
+
+    public static final RegistryObject<EntityType<GlassSnowballEntity>> GLASS_SNOWBALL =registerTool(GlassSnowballEntity::new,"glass_snowball");
+    public static final RegistryObject<EntityType<StoneSnowballEntity>> STONE_SNOWBALL =registerTool(StoneSnowballEntity::new,"stone_snowball");
+    public static final RegistryObject<EntityType<IceSnowballEntity>> ICE_SNOWBALL =registerTool(IceSnowballEntity::new,"ice_snowball");
+    public static final RegistryObject<EntityType<IronSnowballEntity>> IRON_SNOWBALL =registerTool(IronSnowballEntity::new,"iron_snowball");
+    public static final RegistryObject<EntityType<GlassSnowballEntity>> GOLD_SNOWBALL =registerTool(GlassSnowballEntity::new,"gold_snowball");
+    public static final RegistryObject<EntityType<ObsidianSnowballEntity>> OBSIDIAN_SNOWBALL =registerTool(ObsidianSnowballEntity::new,"obsidian_snowball");
+    public static final RegistryObject<EntityType<ExplosiveSnowballEntity>> EXPLOSIVE_SNOWBALL =registerTool(ExplosiveSnowballEntity::new,"explosive_snowball");
+    public static final RegistryObject<EntityType<SpectralSnowballEntity>> SPECTRAL_SNOWBALL =registerTool(SpectralSnowballEntity::new,"spectral_snowball");
+    public static final RegistryObject<EntityType<FrozenSnowballEntity>> FROZEN_SNOWBALL =registerTool(FrozenSnowballEntity::new,"frozen_snowball");
+    public static final RegistryObject<EntityType<PowderSnowballEntity>> POWDER_SNOWBALL =registerTool(PowderSnowballEntity::new,"powder_snowball");
+    public static final RegistryObject<EntityType<LightMonsterTrackingSnowballEntity>> LIGHT_MONSTER_TRACKING_SNOWBALL =registerTool(LightMonsterTrackingSnowballEntity::new,"light_monster_tracking_snowball");
+    public static final RegistryObject<EntityType<HeavyMonsterTrackingSnowballEntity>> HEAVY_MONSTER_TRACKING_SNOWBALL =registerTool(HeavyMonsterTrackingSnowballEntity::new,"heavy_monster_tracking_snowball");
+    public static final RegistryObject<EntityType<ExplosiveMonsterTrackingSnowballEntity>> EXPLOSIVE_MONSTER_TRACKING_SNOWBALL =registerTool(ExplosiveMonsterTrackingSnowballEntity::new,"explosive_monster_tracking_snowball");
+    public static final RegistryObject<EntityType<LightPlayerTrackingSnowballEntity>> LIGHT_PLAYER_TRACKING_SNOWBALL =registerTool(LightPlayerTrackingSnowballEntity::new,"light_player_tracking_snowball");
+    public static final RegistryObject<EntityType<HeavyPlayerTrackingSnowballEntity>> HEAVY_PLAYER_TRACKING_SNOWBALL =registerTool(HeavyPlayerTrackingSnowballEntity::new,"heavy_player_tracking_snowball");
+    public static final RegistryObject<EntityType<ExplosivePlayerTrackingSnowballEntity>> EXPLOSIVE_PLAYER_TRACKING_SNOWBALL =registerTool(ExplosivePlayerTrackingSnowballEntity::new,"explosive_player_tracking_snowball");
+    public static final RegistryObject<EntityType<MonsterGravitySnowballEntity>> MONSTER_GRAVITY_SNOWBALL =registerTool(MonsterGravitySnowballEntity::new,"monster_gravity_snowball");//name is changed
+    public static final RegistryObject<EntityType<ProjectileGravitySnowballEntity>> PROJECTILE_GRAVITY_SNOWBALL =registerTool(ProjectileGravitySnowballEntity::new,"projectile_gravity_snowball");//name is changed
+    public static final RegistryObject<EntityType<MonsterRepulsionSnowballEntity>> MONSTER_REPULSION_SNOWBALL =registerTool(MonsterRepulsionSnowballEntity::new,"monster_repulsion_snowball");//name is changed
+    public static final RegistryObject<EntityType<ProjectileRepulsionSnowballEntity>> PROJECTILE_REPULSION_SNOWBALL =registerTool(ProjectileRepulsionSnowballEntity::new,"projectile_repulsion_snowball");//name is changed
+    public static final RegistryObject<EntityType<BlackHoleSnowballEntity>> BLACK_HOLE_SNOWBALL =registerTool(BlackHoleSnowballEntity::new,"black_hole_snowball");
+    public static final RegistryObject<EntityType<SubspaceSnowballEntity>> SUBSPACE_SNOWBALL =registerTool(SubspaceSnowballEntity::new,"subspace_snowball");
+    public static final RegistryObject<EntityType<EnderSnowballEntity>> ENDER_SNOWBALL =registerTool(EnderSnowballEntity::new,"ender_snowball");
+    //GPS Snowball
+
+//    public static final RegistryObject<EntityType<ExplosiveSnowballEntity>> EXPLOSIVE_SNOWBALL =
+//            ENTITY_TYPES.register("explosive_snowball", () -> EntityType.Builder.<ExplosiveSnowballEntity>of(ExplosiveSnowballEntity::new, MobCategory.MISC)
+//                    .sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10)
+//                    .build(new ResourceLocation(Main.MODID, "explosive_snowball").toString()));
+
+    /**
+     * A tool to register entity
+     * @param pFactory
+     * @param name
+     * @return
+     * @param <T>
+     */
+    public static <T extends Entity> RegistryObject<EntityType<T>> registerTool(EntityType.EntityFactory<T> pFactory, String name){
+        return ENTITY_TYPES.register(name, () -> EntityType.Builder.of(pFactory, MobCategory.MISC)
+                .sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10)
+                .build(new ResourceLocation(Main.MODID, name).toString()));
+    }
+
 }
