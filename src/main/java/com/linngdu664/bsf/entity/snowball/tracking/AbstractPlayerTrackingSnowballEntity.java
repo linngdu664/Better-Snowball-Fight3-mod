d1986a4f967817db256c71cf07935034b2301d84
zx1316 <1315862279@qq.com>
2023-07-18 15:20:35 +0800

    重写获取玩家逻辑

---
+++ src/main/java/com/linngdu664/bsf/entity/snowball/tracking/AbstractPlayerTrackingSnowballEntity.java
@@ -0,0 +1,45 @@
+package com.linngdu664.bsf.entity.snowball.tracking;
+
+import com.linngdu664.bsf.entity.BSFSnowGolemEntity;
+import com.linngdu664.bsf.entity.snowball.util.ILaunchAdjustment;
+import com.linngdu664.bsf.util.BSFMthUtil;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+
+public abstract class AbstractPlayerTrackingSnowballEntity extends AbstractTrackingSnowballEntity {
+    public AbstractPlayerTrackingSnowballEntity(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    public AbstractPlayerTrackingSnowballEntity(EntityType<? extends ThrowableItemProjectile> pEntityType, LivingEntity pShooter, Level pLevel, ILaunchAdjustment launchAdjustment) {
+        super(pEntityType, pShooter, pLevel, launchAdjustment);
+    }
+    @Override
+    public double getRange() {
+        return 10;
+    }
+
+    @Override
+    public Entity getTarget() {
+        Vec3 velocity = getDeltaMovement();
+        Level level = level();
+        Entity owner = getOwner();
+        AABB aabb = getBoundingBox().inflate(getRange());
+        List<Player> list = level.getEntitiesOfClass(Player.class, aabb, (p) -> !p.isCreative() && !p.isSpectator() && !p.equals(owner) && !(owner instanceof BSFSnowGolemEntity golem && p.equals(golem.getOwner())) && BSFMthUtil.vec3AngleCos(velocity, new Vec3(p.getX() - getX(), p.getY() - getY(), p.getZ() - getZ())) > 0.5);
+        Entity entity = level.getNearestEntity(list, TargetingConditions.DEFAULT, null, getX(), getY(), getZ());
+        if (entity == null) {
+            List<BSFSnowGolemEntity> list1 = level.getEntitiesOfClass(BSFSnowGolemEntity.class, aabb, (p) -> p.getTarget() != null && (owner instanceof BSFSnowGolemEntity golem && p.getTarget().equals(golem.getOwner()) || !(owner instanceof BSFSnowGolemEntity) && p.getTarget().equals(owner)) && BSFMthUtil.vec3AngleCos(velocity, new Vec3(p.getX() - getX(), p.getY() - getY(), p.getZ() - getZ())) > 0.5);
+            return level.getNearestEntity(list1, TargetingConditions.DEFAULT, null, getX(), getY(), getZ());
+        }
+        return entity;
+    }
+}
--- src/main/java/com/linngdu664/bsf/entity/snowball/tracking/ExplosivePlayerTrackingSnowballEntity.java
+++ src/main/java/com/linngdu664/bsf/entity/snowball/tracking/ExplosivePlayerTrackingSnowballEntity.java
@@ -3,55 +3,29 @@ package com.linngdu664.bsf.entity.snowball.tracking;
 import com.linngdu664.bsf.entity.EntityRegister;
 import com.linngdu664.bsf.entity.snowball.util.ILaunchAdjustment;
 import com.linngdu664.bsf.item.ItemRegister;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
 
-public class ExplosivePlayerTrackingSnowballEntity extends AbstractTrackingSnowballEntity {
+public class ExplosivePlayerTrackingSnowballEntity extends AbstractPlayerTrackingSnowballEntity {
     public ExplosivePlayerTrackingSnowballEntity(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     public ExplosivePlayerTrackingSnowballEntity(LivingEntity pShooter, Level pLevel, ILaunchAdjustment launchAdjustment) {
-        super(EntityRegister.EXPLOSIVE_PLAYER_TRACKING_SNOWBALL.get(), pShooter, pLevel);
-        this.launchAdjustment = launchAdjustment;
+        super(EntityRegister.EXPLOSIVE_PLAYER_TRACKING_SNOWBALL.get(), pShooter, pLevel, launchAdjustment);
     }
-//    public ExplosivePlayerTrackingSnowballEntity(LivingEntity livingEntity, Level level, LaunchFunc launchFunc) {
-//        super(livingEntity, level);
-//        this.setLockFeet(true).setRange(10).setTargetClass(Player.class).setDamage(3).setBlazeDamage(5).setLaunchFrom(launchFunc.getLaunchFrom());
-//        launchFunc.launchProperties(this);
-//        this.setItem(new ItemStack(ItemRegister.EXPLOSIVE_PLAYER_TRACKING_SNOWBALL.get()));
-//    }
-
-//    @Override
-//    public Item getCorrespondingItem() {
-//        return ItemRegister.EXPLOSIVE_PLAYER_TRACKING_SNOWBALL.get();
-//    }
 
     @Override
     protected void onHit(@NotNull HitResult pResult) {
         super.onHit(pResult);
-        if (!level().isClientSide) {
-            if (!isCaught) {
-                handleExplosion(1.5F);
-            }
-            this.discard();
+        if (!level().isClientSide && !isCaught) {
+            handleExplosion(1.5F);
         }
-    }
-
-//    public float getPower() {
-//        return 3.25f;
-//    }
-
-    @Override
-    public boolean canBeCaught() {
-        return true;
     }
 
     @Override
@@ -82,16 +56,6 @@ public class ExplosivePlayerTrackingSnowballEntity extends AbstractTrackingSnowb
     @Override
     public float getSubspacePower() {
         return 3.25F;
-    }
-
-    @Override
-    public double getRange() {
-        return 10;
-    }
-
-    @Override
-    public Class<? extends Entity> getTargetClass() {
-        return Player.class;
     }
 
     @Override
