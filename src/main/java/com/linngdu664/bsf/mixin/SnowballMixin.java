6a4d21ad1f708f0ca9bc9680f9aa749f20556657
zx1316 <1315862279@qq.com>
2023-07-18 21:12:42 +0800

    重写原版雪球实体mixin，用inject代替overwrite

--- src/main/java/com/linngdu664/bsf/mixin/SnowballMixin.java
+++ src/main/java/com/linngdu664/bsf/mixin/SnowballMixin.java
@@ -10,8 +10,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
@@ -25,7 +23,7 @@ import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -53,7 +51,7 @@ public class SnowballMixin extends ThrowableItemProjectile {
     @Override
     protected void onHitBlock(@NotNull BlockHitResult p_37258_) {
         super.onHitBlock(p_37258_);
-        spawnBasicParticles(level());
+        bsf$spawnBasicParticles(level());
     }
 
     // Cancel "broadcastEntityEvent" and discard directly.
@@ -63,45 +61,51 @@ public class SnowballMixin extends ThrowableItemProjectile {
         ci.cancel();
     }
 
-    private boolean isHeadingToSnowball(Player player) {
-        Vec3 speedVec = this.getDeltaMovement().normalize();
-        Vec3 cameraVec = Vec3.directionFromRotation(player.getXRot(), player.getYRot());
-        return Mth.abs((float) (cameraVec.dot(speedVec) + 1.0F)) < 0.2F;
-    }
-
-    /**
-     * @author zx1316
-     * @reason Now player can catch vanilla snowball, and vanilla snowball can knock back player. Also add fancy particles.
-     */
-    @Overwrite
-    protected void onHitEntity(@NotNull EntityHitResult pResult) {
+    @Inject(method = "onHitEntity", at = @At(value = "HEAD"), cancellable = true)
+    private void injectedAtHeadOnHitEntity(EntityHitResult pResult, CallbackInfo ci) {
         Level level = level();
-        if (pResult.getEntity() instanceof LivingEntity entity) {
-            if (entity instanceof Player player && (player.getOffhandItem().is(ItemRegister.GLOVE.get()) &&
-                    player.getUsedItemHand() == InteractionHand.OFF_HAND || player.getMainHandItem().is(ItemRegister.GLOVE.get()) &&
-                    player.getUsedItemHand() == InteractionHand.MAIN_HAND) && player.isUsingItem() && isHeadingToSnowball(player)) {
-                player.getInventory().placeItemBackInInventory(new ItemStack(Items.SNOWBALL, 1), true);
-                if (ItemStack.isSameItem(player.getMainHandItem(), new ItemStack(ItemRegister.GLOVE.get()))) {
-                    player.getMainHandItem().hurtAndBreak(1, player, (e) -> e.broadcastBreakEvent(EquipmentSlot.MAINHAND));
-                } else if (ItemStack.isSameItem(player.getOffhandItem(), new ItemStack(ItemRegister.GLOVE.get()))) {
-                    player.getOffhandItem().hurtAndBreak(1, player, (e) -> e.broadcastBreakEvent(EquipmentSlot.OFFHAND));
-                }
-                level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOW_BREAK, SoundSource.NEUTRAL, 3F, 0.4F / level.getRandom().nextFloat() * 0.4F + 0.8F);
-                if (!level.isClientSide) {
-                    ((ServerLevel) level).sendParticles(ParticleTypes.SNOWFLAKE, this.getX(), this.getY(), this.getZ(), 3, 0, 0, 0, 0.04);
-                }
-                return;
+        if (pResult.getEntity() instanceof Player player && (player.getOffhandItem().is(ItemRegister.GLOVE.get()) &&
+                player.getUsedItemHand() == InteractionHand.OFF_HAND || player.getMainHandItem().is(ItemRegister.GLOVE.get()) &&
+                player.getUsedItemHand() == InteractionHand.MAIN_HAND) && player.isUsingItem() && bsf$isHeadingToSnowball(player)) {
+            player.getInventory().placeItemBackInInventory(Items.SNOWBALL.getDefaultInstance(), true);
+            if (ItemStack.isSameItem(player.getMainHandItem(), ItemRegister.GLOVE.get().getDefaultInstance())) {
+                player.getMainHandItem().hurtAndBreak(1, player, (e) -> e.broadcastBreakEvent(EquipmentSlot.MAINHAND));
+            } else if (ItemStack.isSameItem(player.getOffhandItem(), ItemRegister.GLOVE.get().getDefaultInstance())) {
+                player.getOffhandItem().hurtAndBreak(1, player, (e) -> e.broadcastBreakEvent(EquipmentSlot.OFFHAND));
             }
-            float i = entity instanceof Blaze ? 3.0F : Float.MIN_VALUE;
-            entity.hurt(level.damageSources().thrown(this, this.getOwner()), i);
+            level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOW_BREAK, SoundSource.NEUTRAL, 3F, 0.4F / level.getRandom().nextFloat() * 0.4F + 0.8F);
+            if (!level.isClientSide) {
+                ((ServerLevel) level).sendParticles(ParticleTypes.SNOWFLAKE, this.getX(), this.getY(), this.getZ(), 3, 0, 0, 0, 0.04);
+            }
+            ci.cancel();
         }
-        spawnBasicParticles(level);
     }
 
-    private void spawnBasicParticles(Level level) {
+    @Inject(method = "onHitEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"), cancellable = true)
+    private void injectedBeforeInvokeHurtOnHitEntity(EntityHitResult pResult, CallbackInfo ci) {
+        if (pResult.getEntity() instanceof Player player && !player.isCreative()) {
+            player.hurt(this.damageSources().thrown(this, this.getOwner()), Float.MIN_NORMAL);
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "onHitEntity", at = @At(value = "TAIL"))
+    private void injectedAtTailOnHitEntity(EntityHitResult pResult, CallbackInfo ci) {
+        bsf$spawnBasicParticles(level());
+    }
+
+    @Unique
+    private void bsf$spawnBasicParticles(Level level) {
         if (!level.isClientSide) {
             ((ServerLevel) level).sendParticles(ParticleTypes.ITEM_SNOWBALL, this.getX(), this.getY(), this.getZ(), 8, 0, 0, 0, 0);
             ((ServerLevel) level).sendParticles(ParticleTypes.SNOWFLAKE, this.getX(), this.getY(), this.getZ(), 8, 0, 0, 0, 0.04);
         }
+    }
+
+    @Unique
+    private boolean bsf$isHeadingToSnowball(Player player) {
+        Vec3 speedVec = this.getDeltaMovement().normalize();
+        Vec3 cameraVec = Vec3.directionFromRotation(player.getXRot(), player.getYRot());
+        return Mth.abs((float) (cameraVec.dot(speedVec) + 1.0F)) < 0.2F;
     }
 }
