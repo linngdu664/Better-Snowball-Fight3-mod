7d966965420902d99221eea46b5a7c9b7842e5ac
zx1316 <1315862279@qq.com>
2023-07-19 23:31:02 +0800

    新增效果武器阻塞

---
+++ src/main/java/com/linngdu664/bsf/effect/WeaponJamEffect.java
@@ -0,0 +1,10 @@
+package com.linngdu664.bsf.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class WeaponJamEffect extends MobEffect {
+    protected WeaponJamEffect(MobEffectCategory pCategory, int pColor) {
+        super(pCategory, pColor);
+    }
+}
