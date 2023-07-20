794fd496ab1f65a7eef964554f5a70fd7fe3cc7c
zx1316 <1315862279@qq.com>
2023-07-20 21:44:00 +0800

    优化import

--- src/main/java/com/linngdu664/bsf/item/tool/SnowBlockBlenderItem.java
+++ src/main/java/com/linngdu664/bsf/item/tool/SnowBlockBlenderItem.java
@@ -25,7 +25,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Random;
 
 public class SnowBlockBlenderItem extends AbstractBSFEnhanceableToolItem {
     public SnowBlockBlenderItem() {
