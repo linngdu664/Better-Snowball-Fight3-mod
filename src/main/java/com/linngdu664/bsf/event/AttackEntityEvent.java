e6d6ea8693dce1287d146826cb6ca7e959a73839
zx1316 <1315862279@qq.com>
2023-07-21 17:14:30 +0800

    添加给予武器阻塞效果，修复雪傀儡会得到缓慢和冻结debuff的bug

--- src/main/java/com/linngdu664/bsf/event/AttackEntityEvent.java
+++ src/main/java/com/linngdu664/bsf/event/AttackEntityEvent.java
@@ -1,6 +1,8 @@
 package com.linngdu664.bsf.event;
 
 import com.linngdu664.bsf.Main;
+import com.linngdu664.bsf.effect.EffectRegister;
+import com.linngdu664.bsf.entity.BSFSnowGolemEntity;
 import com.linngdu664.bsf.item.snowball.normal.SmoothSnowballItem;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
@@ -9,6 +11,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.SnowGolem;
 import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
@@ -26,12 +29,15 @@ public class AttackEntityEvent {
         Item item = player.getMainHandItem().getItem();
         if (!player.isSpectator() && entity instanceof LivingEntity target) {
             if (item instanceof SolidBucketItem) {
-                if (target.getTicksFrozen() < 240) {
-                    target.setTicksFrozen(240);
+                if (!(target instanceof BSFSnowGolemEntity) && !(target instanceof SnowGolem)) {
+                    if (target.getTicksFrozen() < 240) {
+                        target.setTicksFrozen(240);
+                    }
+                    target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 1));
+                    target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 2));
+                    target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 150, 1));
                 }
-                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 1));
-                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 2));
-                target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 150, 1));
+                target.addEffect(new MobEffectInstance(EffectRegister.WEAPON_JAM.get(), 80, 1));
                 if (level instanceof ServerLevel serverLevel) {
                     serverLevel.sendParticles(ParticleTypes.ITEM_SNOWBALL, target.getX(), target.getEyeY(), target.getZ(), 16, 0, 0, 0, 0);
                     serverLevel.sendParticles(ParticleTypes.SNOWFLAKE, target.getX(), target.getEyeY(), target.getZ(), 16, 0, 0, 0, 0.04);
@@ -44,11 +50,14 @@ public class AttackEntityEvent {
                     player.getInventory().placeItemBackInInventory(new ItemStack(Items.BUCKET), true);
                 }
             } else if (item instanceof SnowballItem || item instanceof SmoothSnowballItem) {
-                if (target.getTicksFrozen() < 180) {
-                    target.setTicksFrozen(180);
+                if (!(target instanceof BSFSnowGolemEntity) && !(target instanceof SnowGolem)) {
+                    if (target.getTicksFrozen() < 180) {
+                        target.setTicksFrozen(180);
+                    }
+                    target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 1));
+                    target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 30, 1));
                 }
-                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 1));
-                target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 30, 1));
+                target.addEffect(new MobEffectInstance(EffectRegister.WEAPON_JAM.get(), 40, 1));
                 if (!player.getAbilities().instabuild) {
                     player.getItemInHand(InteractionHand.MAIN_HAND).shrink(1);
                 }
