35882e47869ec06923354252ff2e3cef50e3fdea
zx1316 <1315862279@qq.com>
2023-07-25 17:54:12 +0800

    修复背景不透明的bug

--- src/main/java/com/linngdu664/bsf/event/RenderOverlayEvent.java
+++ src/main/java/com/linngdu664/bsf/event/RenderOverlayEvent.java
@@ -34,13 +34,13 @@ public class RenderOverlayEvent {
                 GuiGraphics guiGraphics = event.getGuiGraphics();
                 Window window = event.getWindow();
                 int startPos = window.getHeight() * 3 / 8 / (int) window.getGuiScale();
+                guiGraphics.blit(new ResourceLocation("bsf", "textures/gui/snowball_frame.png"), 0, startPos, 0, 0, 23, 62,23,62);
                 guiGraphics.renderItem(prev, 3, startPos + 3);
                 guiGraphics.renderItem(current, 3, startPos + 23);
                 guiGraphics.renderItem(next, 3, startPos + 43);
                 guiGraphics.drawString(instance.font, String.valueOf(prev.getCount()), 24, startPos + 7, 0xffffffff);
                 guiGraphics.drawString(instance.font, String.valueOf(current.getCount()), 24, startPos + 27, 0xffffffff);
                 guiGraphics.drawString(instance.font, String.valueOf(next.getCount()), 24, startPos + 47, 0xffffffff);
-                guiGraphics.blit(new ResourceLocation("bsf", "textures/gui/snowball_frame.png"), 0, startPos, 0, 0, 23, 62,23,62);
             }
         }
     }
