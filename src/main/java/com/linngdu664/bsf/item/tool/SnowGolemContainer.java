95767cacb0c3229811648b3cbae57a902e95a1a3
zx1316 <1315862279@qq.com>
2023-07-31 22:05:07 +0800

    添加雪傀儡收纳器

--- src/main/java/com/linngdu664/bsf/item/tool/SnowGolemContainer.java
+++ src/main/java/com/linngdu664/bsf/item/tool/SnowGolemContainer.java
@@ -4,9 +4,7 @@ import com.linngdu664.bsf.entity.BSFSnowGolemEntity;
 import com.linngdu664.bsf.registry.EntityRegister;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
@@ -25,23 +23,11 @@ public class SnowGolemContainer extends Item {
         CompoundTag tag = itemStack.getOrCreateTag();
         if (tag.getBoolean("HasGolem")) {
             if (!level.isClientSide) {
-                byte status = tag.getByte("Status");
                 BSFSnowGolemEntity snowGolem = EntityRegister.BSF_SNOW_GOLEM.get().create(level);
-                snowGolem.setTame(true);
-                snowGolem.setOwnerUUID(pContext.getPlayer().getUUID());
-                snowGolem.setOrderedToSit(status == 0);
-                snowGolem.setStatus(status);
-                snowGolem.setUseLocator(tag.getBoolean("UseLocator"));
-                snowGolem.setAmmo(ItemStack.of(tag.getCompound("Ammo")));
-                snowGolem.setWeapon(ItemStack.of(tag.getCompound("Weapon")));
-                snowGolem.setEnhance(tag.getBoolean("Enhance"));
-                snowGolem.setStyle(tag.getByte("Style"));
+                snowGolem.readAdditionalSaveData(tag);
                 BlockPos blockPos = pContext.getClickedPos();
                 snowGolem.moveTo(blockPos.getX() + 0.5, blockPos.getY() + 1, blockPos.getZ() + 0.5, 0.0F, 0.0F);
                 level.addFreshEntity(snowGolem);
-                if (tag.contains("UUID") && ((ServerLevel) level).getEntity(tag.getUUID("UUID")) instanceof LivingEntity livingEntity) {
-                    snowGolem.setTarget(livingEntity);
-                }
                 tag.putBoolean("HasGolem", false);
             }
             return InteractionResult.SUCCESS;
