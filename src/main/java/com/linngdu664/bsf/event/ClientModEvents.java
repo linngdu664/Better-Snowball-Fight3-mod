00e409563402c09c34d506f38ae63d40987f2536
zx1316 <1315862279@qq.com>
2023-07-31 22:06:26 +0800

    添加收纳器贴图切换

--- src/main/java/com/linngdu664/bsf/event/ClientModEvents.java
+++ src/main/java/com/linngdu664/bsf/event/ClientModEvents.java
@@ -63,7 +63,7 @@ public class ClientModEvents {
                     -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
             ItemProperties.register(ItemRegister.LARGE_SNOWBALL_TANK.get(), new ResourceLocation("snowball"), (itemStack, world, livingEntity, num) -> getSnowballId(ForgeRegistries.ITEMS.getValue(new ResourceLocation(Main.MODID, itemStack.getTag().getString("Snowball")))));
             ItemProperties.register(ItemRegister.SNOWBALL_TANK.get(), new ResourceLocation("snowball"), (itemStack, world, livingEntity, num) -> getSnowballId(ForgeRegistries.ITEMS.getValue(new ResourceLocation(Main.MODID, itemStack.getTag().getString("Snowball")))));
-            ItemProperties.register(ItemRegister.SNOW_GOLEM_CONTAINER.get(), new ResourceLocation("has_golem"),  (itemStack, world, livingEntity, num) -> itemStack.getOrCreateTag().getBoolean("HasGolem") ? 1.0F : 0.0F);
+            ItemProperties.register(ItemRegister.SNOW_GOLEM_CONTAINER.get(), new ResourceLocation("has_golem"), (itemStack, world, livingEntity, num) -> itemStack.getOrCreateTag().getBoolean("HasGolem") ? 1.0F : 0.0F);
         });
     }
 
