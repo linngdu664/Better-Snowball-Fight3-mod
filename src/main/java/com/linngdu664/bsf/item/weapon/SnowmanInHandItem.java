4c573e68d3bf1f64030de29fee4d7d3fa2203d7f
zx1316 <1315862279@qq.com>
2024-01-28 15:22:18 +0800

    手持雪人可以附魔了

--- src/main/java/com/linngdu664/bsf/item/weapon/SnowmanInHandItem.java
+++ src/main/java/com/linngdu664/bsf/item/weapon/SnowmanInHandItem.java
@@ -18,6 +18,7 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -128,7 +129,7 @@ public class SnowmanInHandItem extends Item {
 
     @Override
     public int getEnchantmentValue(ItemStack stack) {
-        return 0;
+        return 25;
     }
 
     @Override
