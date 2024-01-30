daea197bf2a10eb6dfe36d749f3a7e4a804ea6e7
zx1316 <1315862279@qq.com>
2024-01-30 22:27:40 +0800

    同队高亮显示mixin

---
+++ src/main/java/com/linngdu664/bsf/mixin/MinecraftMixin.java
@@ -0,0 +1,26 @@
+package com.linngdu664.bsf.mixin;
+
+import com.linngdu664.bsf.item.tool.TeamLinkerItem;
+import com.linngdu664.bsf.network.TeamMembersToClient;
+import com.mojang.blaze3d.platform.WindowEventHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.thread.ReentrantBlockableEventLoop;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Minecraft.class)
+public abstract class MinecraftMixin extends ReentrantBlockableEventLoop<Runnable> implements WindowEventHandler, net.minecraftforge.client.extensions.IForgeMinecraft {
+    public MinecraftMixin(String pName) {
+        super(pName);
+    }
+
+    @Inject(method = "shouldEntityAppearGlowing", at = @At(value = "HEAD"), cancellable = true)
+    private void shouldEntityAppearGlowing(Entity pEntity, CallbackInfoReturnable<Boolean> cir) {
+        if (TeamLinkerItem.shouldShowHighlight && TeamMembersToClient.staticMembers.contains(pEntity.getUUID())) {
+            cir.setReturnValue(true);
+        }
+    }
+}
