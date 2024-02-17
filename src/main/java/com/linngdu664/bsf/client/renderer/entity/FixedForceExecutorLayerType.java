69d5ab2c130dda8162ee197c55824b7cac9a0963
zx1316 <89696903+zx1316@users.noreply.github.com>
2024-02-17 12:57:44 +0800

    Delete src directory

--- src/main/java/com/linngdu664/bsf/client/renderer/entity/FixedForceExecutorLayerType.java
+++
@@ -1,8 +0,0 @@
-package com.linngdu664.bsf.client.renderer.entity;
-
-public enum FixedForceExecutorLayerType {
-    MONSTER_GRAVITY,
-    MONSTER_REPULSION,
-    PROJECTILE_GRAVITY,
-    PROJECTILE_REPULSION
-}
