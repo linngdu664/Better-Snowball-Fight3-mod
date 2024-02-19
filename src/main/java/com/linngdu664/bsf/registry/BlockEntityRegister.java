385b7017612e43f4e6627d5c4bf10cc3d9b01e45
zx1316 <1315862279@qq.com>
2024-02-19 15:11:26 +0800

    调整结构

--- src/main/java/com/linngdu664/bsf/registry/BlockEntityRegister.java
+++ src/main/java/com/linngdu664/bsf/registry/BlockEntityRegister.java
@@ -3,7 +3,6 @@ package com.linngdu664.bsf.registry;
 
 import com.linngdu664.bsf.Main;
 import com.linngdu664.bsf.block.entity.CriticalSnowEntity;
-import com.linngdu664.bsf.registry.BlockRegister;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
