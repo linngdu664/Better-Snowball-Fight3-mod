43bc270ee4a2e69aa128b24af829ea1069051f65
Linngdu664 <664774652@qq.com>
2024-02-22 23:08:28 +0800

    添加力学雪球消失音效

--- src/main/java/com/linngdu664/bsf/entity/executor/AbstractFixedForceExecutor.java
+++ src/main/java/com/linngdu664/bsf/entity/executor/AbstractFixedForceExecutor.java
@@ -4,6 +4,8 @@ import com.linngdu664.bsf.registry.ParticleRegister;
 import com.linngdu664.bsf.util.ParticleUtil;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
@@ -23,6 +25,7 @@ public abstract class AbstractFixedForceExecutor extends AbstractForceExecutor {
         super.remove(pReason);
         Level level = level();
         if (!level.isClientSide) {
+            level.playSound(null, this.getX(), this.getY(), this.getZ(), SoundEvents.ITEM_FRAME_REMOVE_ITEM, SoundSource.PLAYERS, 1.0F, 1.0F / (level.getRandom().nextFloat() * 0.4F + 1.2F) + 0.5F);
             ((ServerLevel) level).sendParticles(ParticleTypes.DRAGON_BREATH, this.getX(), this.getY(), this.getZ(), 10, 0, 0, 0, 0.06);
         }
     }
